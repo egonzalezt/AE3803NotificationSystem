c99ad265e097f88c9f73c357dd5a5c1595959115
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-12 18:20:30 -0500

    fix(dotnet): add env support

--- AE3803Notification/Program.cs
+++ AE3803Notification/Program.cs
@@ -3,6 +3,7 @@ using AE3803Notification.Extensions;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddServices(builder.Configuration);
 builder.Configuration.AddEnvironmentVariables();
+
 var host = builder.Build();
 
 host.Run();
