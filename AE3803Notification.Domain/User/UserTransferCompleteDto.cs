b6758d3386c5be2cbedd475dc11ea27ea05e6e81
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-20 18:24:58 -0500

    feat(transfer): support user transfer to external operator

---
+++ AE3803Notification.Domain/User/UserTransferCompleteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AE3803Notification.Domain.User;
+
+public class UserTransferCompleteDto
+{
+    public Guid TicketId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
